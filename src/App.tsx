a04c6e664a5b42fb8fa27540a1dd8e56243e7418
CamilaMoraisss <camila.morais@estudante.ifms.edu.br>
2023-08-31 08:24:56 -0400

    meu commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import Header from './componentes/header/Header'
-import Footer from './componentes/footer/Footer'
-import Filme from './componentes/filme/Filme'
 import './App.css'
+import Header from './componentes/header/Header'
+import Filme from './componentes/main/Main'
+import Footer from './componentes/footer/Footer'
+
 
 function App() {
   return (
     <div>
          <Header/>
-        <Footer/>
-        <Filme/>
-    </div>
+         <Filme/> 
+         <Filme/>  
+         <Footer/>
+         </div>
   )
 }
 
--- src/componentes/filme/FIlme.tsx
+++ src/componentes/filme/FIlme.tsx
@@ -1,10 +1,13 @@
-
-import Imagem from '..//img/barbie.png'
-function Filme (){
+import Imagem from "./../../assets/barbie.png"
+import './Filme.css'
+function Filme(){
     return(
         <div className="filme-content">
-            Filme
-            <img src= {Imagem} className = "barbie" alt="" />
+            <img className="img-filme" src={Imagem} alt="" />
+            <div className="text-barbie">
+                <h1>Barbie</h1>
+                <p className="sinopse">Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.</p>
+            </div>
         </div>
     )
 }
