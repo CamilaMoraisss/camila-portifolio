1fc3cd888607efbffc72e5d693360ca8b32c6821
CamilaMoraisss <camila.morais@estudante.ifms.edu.br>
2023-08-31 08:16:43 -0400

    testando meu commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Header from './componentes/header/Header'
 import Footer from './componentes/footer/Footer'
-import Filme from './componentes/filme/FIlme'
-
+import Filme from './componentes/filme/Filme'
 import './App.css'
 
 function App() {
--- src/componentes/filme/FIlme.tsx
+++ src/componentes/filme/FIlme.tsx
@@ -1,9 +1,11 @@
-function Filme(){
+
+import Imagem from '..//img/barbie.png'
+function Filme (){
     return(
         <div className="filme-content">
             Filme
+            <img src= {Imagem} className = "barbie" alt="" />
         </div>
     )
 }
-
 export default Filme
