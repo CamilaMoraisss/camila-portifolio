c659e12b53561e0fcae1b8a4b9649a2bc4037012
CamilaMoraisss <camila.morais@estudante.ifms.edu.br>
2023-09-12 09:41:24 -0400

    meu commit

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,22 +1,25 @@
 
-
-
-import Filme from '../filme/Filme.tsx'
+import { useState } from 'react'
+import Filme from './../filme/Filme'
 import './Main.css'
-export default function Main(){
-    return(
-<>
-<div className='campo_pesquisa'>
-    <input type="text" className='botao_pesquisa' placeholder='Pesquise um Filme' />
-    <p>Digitado:</p>
+export default function Main() {
+   // let textodigitado = 'Barbie'
+   const [textodigitado, funcaoMudaVariavel] =  useState("Barbie")
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+        console.log(e.target.value)
+        
+        funcaoMudaVariavel(e.target.value)
+    }
+    return (
+        <>
+            <div className="campo_pesquisa">
+                <p>Busque um filme</p>
+                <input type="text" className='botao_pesquisa' placeholder='Pesquise um Filme'onChange={TrataTexto} /><p>Digitado: {textodigitado} </p>
 </div>
 
 
         
         <main className="content-main">
-    
-            
-            
             <Filme titulo='Barbie Live Action' sinopse='No fabuloso live-action da boneca mais famosa do mundo,
              acompanhamos o dia a dia em Barbieland - o mundo mágico das Barbies, onde todas as versões da boneca
               vivem em completa harmonia e suas únicas preocupações são encontrar as melhores roupas para passear
