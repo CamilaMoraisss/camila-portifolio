d722be14691f8eade1d144e1ef77ad706ba4c788
CamilaMoraisss <camila.morais@estudante.ifms.edu.br>
2023-09-20 08:10:58 -0400

    Primeiro commit

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -14,22 +14,27 @@ export default function Main() {
 
     const projetos:ProjetoType[] = [
         {
-            titulo:'Podcast - POD ISSO No IF?',
-            descricao:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            titulo:'POD ISSO NO IF?',
+            descricao:"Projeto de Extensão de um podcast para o IFMS, desenvolvido pelo professor Diego Barros e algumas alunas e outros professores. ",
             imagem:'/barbie.png'
         },
         {
-            titulo:'Filme Barbie',
-            descricao:'Depois de ser expulsa da Barbieland por ser.',
+            titulo:'INICIAÇÃO CIENTÍFICA',
+            descricao:'Esse projeto consiste em um zoenamento agrícola da cana-de-açúcar em cenários de mudanças climáticas no estado do Mato Grosso do Sul. ',
             imagem:'/KEN.png'
         },
         {
-            titulo:'Filme Barbie',
+            titulo:'NAVITRINE',
             descricao:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
             imagem:'/boneca.jpg'
         },
         {
-            titulo:'Barbie',
+            titulo:'GAVECH',
+            descricao:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        },
+        {
+            titulo:'IDENTIDADE VISUAL - NITA',
             descricao:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
             imagem:'/barbie.png'
         },
