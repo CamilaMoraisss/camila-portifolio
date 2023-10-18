287cdd13d4db532293bd84bad9608b94c271237b
CamilaMoraisss <camila.morais@estudante.ifms.edu.br>
2023-10-18 08:06:55 -0400

    meu commitinho

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,12 +1,16 @@
-import {useState} from 'react'
-import './Contato.css'
-export default function Contato(){
-    const [] = useState("")
-
-return(
-    <>
+import "./Contato.css";
+function Contato(){
+    return(
+        <>
+       
+        <div className="Formulario">
+            <input className="nome" type="text" placeholder="Nome:"/>
+            <input className="telefone" type="text" placeholder="Telefone:*"/>
+            <input className="campo-mensagem" type="text" placeholder="Digite aqui:"/>
+            <button className="button-envio">Enviar</button>
+        </div>
+        </>
+    )
     
-    
-    </>
-)
 }
+export default Contato
